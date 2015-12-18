294264576e31fe7addb30abd4ce68281a466c20d
RealLukeMartin <lukemartinwebdev@gmail.com>
2015-12-17 23:22:10 -0600

    clean up

--- client/js/osc.js
+++ client/js/osc.js
@@ -1,41 +1,36 @@
-	var drum = new Wad({ 
-		source : 'dubbeat.wav', 
-		label : 'D', 
-		loop  : true
-	});
-	var saw = new Wad({
-	  source : 'sawtooth',
-		volume  : 0.8,
-		wait    : 0,     // Time in seconds between calling play() and actually triggering the note.
-		pitch   : 'C2',  // A4 is 440 hertz.
-		label   : 'C',   // A label that identifies this note.
-		env     : {hold : 3.14},
-		panning : [1, -1, 10],
-		filter  : {frequency : 900},
-		tremolo : {
-			shape     : 'sine',
-			magnitude : 10,
-			speed     : 2,
-			attack    : 0  
-		}
-  })
-  saw.on = 0;
-  drum.on = 0;
+var drum = new Wad({
+  source : 'dubbeat.wav',
+  label : 'D',
+  loop  : true
+});
+var saw = new Wad({
+  source : 'sawtooth',
+  volume  : 0.8,
+  wait    : 0,     // Time in seconds between calling play() and actually triggering the note.
+  pitch   : 'C2',  // A4 is 440 hertz.
+  label   : 'C',   // A label that identifies this note.
+  env     : {hold : 3.14},
+  panning : [1, -1, 10],
+  filter  : {frequency : 900},
+  tremolo : {
+    shape     : 'sine',
+    magnitude : 10,
+    speed     : 2,
+    attack    : 0
+  }
+});
+saw.on = 0;
+drum.on = 0;
 
-	Template.synth.events({
-	  'click button': function () {
-			if ( saw.on === 0 ) {
-				saw.play({ volume : 0.8 });
-				saw.on = 1;
-			}
-			else {
-				saw.stop();
-				saw.on = 0;
-			}
-	  }
-	});
-	Template.drums.events({
+Template.play.events({
   'click button': function () {
-    drum.play({ loop  : true });
+    if ( saw.on === 0 ) {
+      saw.play({ volume : 0.8 });
+      saw.on = 1;
+    }
+    else {
+      saw.stop();
+      saw.on = 0;
+    }
   }
 });
