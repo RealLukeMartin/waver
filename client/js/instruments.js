c673309789fa5ec920343f17f51068ff2d9113f3
RealLukeMartin <lukemartinwebdev@gmail.com>
2015-12-24 22:30:23 -0600

    set up tremolo modulation

--- client/js/instruments.js
+++ client/js/instruments.js
@@ -13,12 +13,12 @@ var oscillator = new Wad({
       q         : 1
   },
   label   : 'C',
-  env     : {hold : 3.14},
+  env     : {hold : 9001},
   panning : [1, -1, 10],
   tremolo : {
     shape     : 'sine',
     magnitude : 10,
-    speed     : 4,
+    speed     : 3,
     attack    : 0
   }
 })
@@ -27,11 +27,12 @@ oscillator.on = 0;
 
 Template.play.events({
   'click button': function () {
+
     var clickedInstrument = event.target.id;
     var instrumentObject = eval(clickedInstrument);
     var $oscillatorFilter = $('#osc-filter');
     if ( instrumentObject.on === 0 ) {
-      instrumentObject.play({ 
+      instrumentObject.play({
         volume : 0.8,
         filter  : {
             type      : 'lowpass',
@@ -52,16 +53,20 @@ Template.play.events({
 Template.modify.events({
   'mouseup input': function() {
     var effectInput = event.target;
+    var bridgeFrequency = $('#osc-filter').val();
+
     if (oscillator.on === 1){
+      if (effectInput.id == 'osc-speed') {
+        oscillator.tremolo.speed = effectInput.value;
+        console.log(oscillator.tremolo.speed);
+      }
       console.log(event.target.id);
       oscillator.stop();
-      oscillator.play({
-        filter  : {
-            type      : 'lowpass',
-            frequency : effectInput.value,
-            q         : 1
+      oscillator.play({ 
+        filter : {
+          frequency : $('#osc-filter').val()
         }
-      })
+      });
     }
   }
 });
